f29c00ff5f7572920c17d2179dec0686ce1fc1e8
Pranit Bauva <pranit@bauva.com>
2022-09-12 22:38:30 +0530

    nginx: start an nginx service
    Two instances are up and nginx has them as upstream to do routing
    requests by round robin.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
+
+@app.middleware("http")
+async def log_request_response(request: Request, call_next):
+    print(f"{request.method} {request.client.host}:{request.url.port} {request.url.path}")
+    print(f"Request Headers: {request.headers}")
+    response = await call_next(request)
+    print(f"Response Headers: {response.headers}")
+    return response
 
 @app.get("/")
 async def hello():
